a5db332436d120c043187a067e5cdde6c45920e6
Daniel Szalay <sza.dan.uj@gmail.com>
2017-07-16 15:15:22 +0200

    turn publics into privates in methods and finals

--- src/main/java/RegExExperiments/Validator.java
+++ src/main/java/RegExExperiments/Validator.java
@@ -5,9 +5,9 @@ import java.util.regex.Pattern;
 
 public class Validator {
 
-  public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+  private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
       Pattern.compile("^[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
-  public static final Pattern VALID_PASSWORD_REGEX =
+  private static final Pattern VALID_PASSWORD_REGEX =
       Pattern.compile("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!#$%&'()*+,-./:;<=>?@^_`{|}~])(?=\\S+$).{8,}");
 
   public static void main(String[] args) {
@@ -35,24 +35,24 @@ public class Validator {
     System.out.println(easyPasswordValidation(whiteSpace));
   }
 
-  public static boolean validateEmail(String email) {
+  private static boolean validateEmail(String email) {
     Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
     return matcher.find();
   }
 
-  public static boolean validatePassword(String password) {
+  private static boolean validatePassword(String password) {
     Matcher matcher = VALID_PASSWORD_REGEX.matcher(password);
     return matcher.find();
   }
 
-  public static String easyEmailValidation(String email) {
+  private static String easyEmailValidation(String email) {
     if (validateEmail(email)) {
       return email + " - Valid email address.";
     }
     return email + " - The Email Address is in an invalid format.";
   }
 
-  public static String easyPasswordValidation(String password) {
+  private static String easyPasswordValidation(String password) {
     if (validatePassword(password)) {
       return password + " - Valid password.";
     } else if (containsWhitespace(password)) {
