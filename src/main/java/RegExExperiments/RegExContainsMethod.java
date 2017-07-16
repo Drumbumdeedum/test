6ab3cd4db32b2f08146c64a41e6e38d977b01587
Daniel Szalay <sza.dan.uj@gmail.com>
2017-07-16 11:44:53 +0200

    add contains method with RegEx

--- src/main/java/RegExExperiments/RegExContainsMethod.java
+++ src/main/java/RegExExperiments/RegExContainsMethod.java
@@ -1,8 +1,23 @@
 package RegExExperiments;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegExContainsMethod {
 
   public static void main(String[] args) {
 
+    String mixedCase = "Example  String";
+    String upperCase = "EXAMPLE STRING";
+    String lowerCase = "example string";
+
+    Pattern pattern = Pattern.compile("(?=.*[a-z])(?=\\S+$)");
+    Matcher matcherMixed = pattern.matcher(mixedCase);
+    Matcher matcherUpper = pattern.matcher(upperCase);
+    Matcher matcherLower = pattern.matcher(lowerCase);
+
+    System.out.println(mixedCase + " contains lowercase letters: " + matcherMixed.find());
+    System.out.println(upperCase + " contains lowercase letters: " + matcherUpper.find());
+    System.out.println(lowerCase + " contains lowercase letters: " + matcherLower.find());
   }
 }
