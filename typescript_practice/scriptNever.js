dde4b11261d502030e8b07e215f524da85301ac6
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-23 23:16:59 +0200

    add nullable examples

--- typescript_practice/scriptNever.js
+++ typescript_practice/scriptNever.js
@@ -3,3 +3,10 @@
 function neverReturn() {
     throw new Error('An error');
 }
+// nullable types
+var canBeNUll = 12;
+canBeNUll = null;
+var canAlsoBeNull;
+canAlsoBeNull = null;
+var canThisBeAny = null;
+canThisBeAny = 12;
--- typescript_practice/scriptNever.ts
+++ typescript_practice/scriptNever.ts
@@ -1,5 +1,12 @@
 // never returns anything
-
 function neverReturn(): never {
   throw new Error('An error');
 }
+
+// nullable types
+let canBeNUll: number | null = 12;
+canBeNUll = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
+let canThisBeAny = null;
+canThisBeAny = 12;
