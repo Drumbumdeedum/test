0591e673de3c4f1e1112ec71a9ff35f362e434a1
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-26 11:12:20 +0200

    add spread operator example

---
+++ typescript_practice/restAndSpread.ts
@@ -0,0 +1,6 @@
+// SPREAD operator (...)
+// ... spreads out arrays into single values
+// turns array of numbers into list of numbers
+
+const numbers = [1, 10, 99, -5];
+console.log(Math.max(...numbers));
